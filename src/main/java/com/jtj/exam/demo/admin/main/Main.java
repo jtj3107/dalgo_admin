780ef32a14d934600f0c49ba6b9ef4582304faf9
jtj3107 <jtj3926@gmail.com>
2021-11-09 20:59:01 +0900

    메인페이지 임시구현

---
+++ src/main/java/com/jtj/exam/demo/admin/main/Main.java
@@ -0,0 +1,16 @@
+package com.jtj.exam.demo.admin.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Main {
+	@RequestMapping("/admin")
+	public String showMain() {
+		return "admin/main";
+	}	
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:admin/main";
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/admin/main.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<c:set var="pageTitle" value="메인" />
+<%@ include file="common/head.jspf"%>
+
+<div>dd</div>
+
+<%@ include file="common/foot.jspf"%>
